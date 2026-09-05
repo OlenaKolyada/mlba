87074079bfa79fd9353bf28635bf2db457c653f6
OlenaKolyada <olena.kolyada@proton.me>
2026-09-05 23:21:49 +0200

    Non-breaking space

--- front-page.php
+++ front-page.php
@@ -64,42 +64,42 @@ get_header();
 				<div class="section-content-image">
 					<figure class="frame">
                         <a href="<?php echo get_template_directory_uri();?>/assets/images/main/roman-mikhalev-et-corinne-lanssens.jpg" class="popup-image" title="Corinne Lanssens et Roman Mikhalev, les fondateurs de Mikhalev Lanssens Ballet Academy">
-                            <img class="section-photo" src="<?php echo get_template_directory_uri();?>/assets/images/main/roman-mikhalev-et-corinne-lanssens.png"
+                            <img class="section-photo" src="<?php echo get_template_directory_uri();?>/assets/images/main/roman-mikhalev-et-corinne-lanssens.jpg"
                                  alt="Corinne Lanssens et Roman Mikhalev, les fondateurs de Mikhalev Lanssens Ballet Academy"
                                  title="Corinne Lanssens et Roman Mikhalev, les fondateurs de Mikhalev Lanssens Ballet Academy" />
                         </a>
 					</figure>
-                    <div class="figcaption">Corinne Lanssens et Roman Mikhalev, les fondateurs de Mikhalev Lanssens Ballet Academy</div>
+                    <div class="figcaption">Corinne Lanssens et&nbsp;Roman Mikhalev, les&nbsp;fondateurs de&nbsp;Mikhalev Lanssens Ballet Academy</div>
 				</div>
 				<div class="section-content-text">
 					<div class="section-text">
 						<h2>A propos de nous</h2>
-						<p>Au sein de la <strong>Mikhalev Lanssens Ballet Academy</strong>, les professeurs placent la richesse de
-							leur parcours et de leur expérience professionnelle au service des élèves, dans le respect du
-							rythme et de la singularité de chacun, dans un esprit de transmission bienveillante et
-							engagée. La danse n’étant pas seulement une technique, nous la défendons comme un art
-							avant tout, et c’est ainsi que nous voulons la transmettre&nbsp;: vivante, sensible, sincère et ainsi
-							offrir à nos élèves une expérience, un langage, une liberté.</p>
+						<p>Au sein de&nbsp;la&nbsp;<strong>Mikhalev Lanssens Ballet Academy</strong>, les&nbsp;professeurs placent la&nbsp;richesse
+                            de&nbsp;leur&nbsp;parcours et&nbsp;de&nbsp;leur&nbsp;expérience professionnelle au&nbsp;service des&nbsp;élèves, dans&nbsp;le&nbsp;respect
+                            du&nbsp;rythme et&nbsp;de&nbsp;la&nbsp;singularité de&nbsp;chacun, dans&nbsp;un&nbsp;esprit de&nbsp;transmission bienveillante
+                            et&nbsp;engagée. La&nbsp;danse n’étant pas&nbsp;seulement une&nbsp;technique, nous la&nbsp;défendons comme
+                            un&nbsp;art&nbsp;avant tout, et&nbsp;c’est ainsi que&nbsp;nous voulons la&nbsp;transmettre&nbsp;: vivante, sensible, sincère et&nbsp;ainsi
+							offrir à&nbsp;nos&nbsp;élèves une&nbsp;expérience, un&nbsp;langage, une&nbsp;liberté.</p>
 					</div>
 					<div class="three-items-wrap">
 						<div class="three-items-row">
 							<div class="three-items-item">
 								<img class="icon" src="<?php echo get_template_directory_uri();?>/assets/icons/star.svg" alt="">
 								<h4>Excellence</h4>
-								<p class="three-items-text">Grâce à une longue expérience (Danseuse et Danseur Étoile de l’Opéra National de
-									Bordeaux), richesse et expérience pédagogique</p>
+								<p class="three-items-text">Grâce à&nbsp;une&nbsp;longue expérience (Danseuse et&nbsp;Danseur Étoile de&nbsp;l’Opéra National
+                                    de&nbsp;Bordeaux), richesse et&nbsp;expérience pédagogique</p>
 							</div>
 							<div class="three-items-item">
 								<img class="icon" src="<?php echo get_template_directory_uri();?>/assets/icons/star.svg" alt="">
 								<h4>Atmosphère</h4>
-								<p class="three-items-text">Un cadre basé sur le respect, la bienveillance, la qualité de l’enseignement,
-									propices à l’épanouissement de chacun</p>
+								<p class="three-items-text">Un&nbsp;cadre basé sur&nbsp;le&nbsp;respect, la&nbsp;bienveillance, la&nbsp;qualité de&nbsp;l’enseignement,
+									propices à&nbsp;l’épanouissement de&nbsp;chacun</p>
 							</div>
 							<div class="three-items-item">
 								<img class="icon" src="<?php echo get_template_directory_uri();?>/assets/icons/star.svg" alt="">
 								<h4>Diversité</h4>
-								<p class="three-items-text">Danse Classique (méthode française et méthode Vaganova) et Contemporaine, pour se découvrir,
-									s’éveiller, s’enrichir et partager</p>
+								<p class="three-items-text">Danse Classique (méthode française et&nbsp;méthode Vaganova) et&nbsp;Contemporaine, pour&nbsp;se&nbsp;découvrir,
+									s’éveiller, s’enrichir et&nbsp;partager</p>
 							</div>
 						</div>
 					</div>
@@ -116,29 +116,29 @@ get_header();
 				<div class="section-content-text">
 					<div class="section-text">
 						<h2>Pourquoi pratiquer la&nbsp;Danse&nbsp;?</h2>
-						<p>La danse est bien plus qu’une activité artistique&nbsp;: c’est une véritable école de vie.
-							Elle développe la discipline, la rigueur, la confiance en soi et la gestion des émotions.
-							En favorisant l’écoute, le respect des autres et le travail en groupe, elle enseigne
-							aussi des valeurs humaines essentielles. À travers l’expression du corps et l’ouverture
-							à la culture, elle cultive la sensibilité et la créativité. Danser, c’est apprendre
-							à se connaître, à persévérer et à grandir en harmonie avec les autres.</p>
+						<p>La danse est bien plus qu’une&nbsp;activité artistique&nbsp;: c’est une&nbsp;véritable école de&nbsp;vie.
+							Elle&nbsp;développe la&nbsp;discipline, la&nbsp;rigueur, la&nbsp;confiance en&nbsp;soi et&nbsp;la&nbsp;gestion des&nbsp;émotions.
+							En&nbsp;favorisant l’écoute, le&nbsp;respect des&nbsp;autres et&nbsp;le&nbsp;travail en&nbsp;groupe, elle enseigne
+							aussi des&nbsp;valeurs humaines essentielles. À&nbsp;travers l’expression du&nbsp;corps et&nbsp;l’ouverture
+							à&nbsp;la&nbsp;culture, elle cultive la&nbsp;sensibilité et&nbsp;la&nbsp;créativité. Danser, c’est&nbsp;apprendre
+							à&nbsp;se&nbsp;connaître, à&nbsp;persévérer et&nbsp;à&nbsp;grandir en&nbsp;harmonie avec les&nbsp;autres.</p>
 					</div>
 					<div class="three-items-wrap">
 						<div class="three-items-row">
 							<div class="three-items-item">
 								<img class="icon" src="<?php echo get_template_directory_uri();?>/assets/icons/star.svg" alt="">
 								<h4>Amélioration</h4>
-								<p class="three-items-text">de la posture, de la souplesse, et de la tonicité musculaire</p>
+								<p class="three-items-text">de&nbsp;la&nbsp;posture, de&nbsp;la&nbsp;souplesse, et&nbsp;de&nbsp;la&nbsp;tonicité musculaire</p>
 							</div>
 							<div class="three-items-item">
 								<img class="icon" src="<?php echo get_template_directory_uri();?>/assets/icons/star.svg" alt="">
 								<h4>Meilleure</h4>
-								<p class="three-items-text">conscience corporelle, maîtrise du corps, et endurance à l’effort</p>
+								<p class="three-items-text">conscience corporelle, maîtrise du&nbsp;corps, et&nbsp;endurance à&nbsp;l’effort</p>
 							</div>
 							<div class="three-items-item">
 								<img class="icon" src="<?php echo get_template_directory_uri();?>/assets/icons/star.svg" alt="">
 								<h4>Développement</h4>
-								<p class="three-items-text">de la confiance en soi, de la mémoire, de la discipline, et de la créativité</p>
+								<p class="three-items-text">de&nbsp;la&nbsp;confiance en&nbsp;soi, de&nbsp;la&nbsp;mémoire, de&nbsp;la&nbsp;discipline, et&nbsp;de&nbsp;la&nbsp;créativité</p>
 							</div>
 						</div>
 					</div>
@@ -167,10 +167,10 @@ get_header();
 				<div class="section-content-center">
 					<div class="section-text">
 						<h2>Section Loisir</h2>
-						<p>Que vous soyez enfant ou adulte, amateur curieux ou passionné, les cours
-							Loisirs de la <strong>Mikhalev Lanssens Ballet Academy</strong> offrent un apprentissage
-							de qualité dans un cadre bienveillant. Dansez pour le plaisir, progressez à votre
-							rythme, et vivez pleinement votre passion&nbsp;!</p>
+						<p>Que vous soyez enfant ou&nbsp;adulte, amateur curieux ou&nbsp;passionné, les&nbsp;cours
+							Loisirs de&nbsp;la&nbsp;<strong>Mikhalev Lanssens Ballet Academy</strong> offrent un&nbsp;apprentissage
+							de&nbsp;qualité dans&nbsp;un&nbsp;cadre bienveillant. Dansez pour&nbsp;le&nbsp;plaisir, progressez à&nbsp;votre
+							rythme, et&nbsp;vivez pleinement votre&nbsp;passion&nbsp;!</p>
 					</div>
 					<div class="cards five">
 						<div class="card-item">
@@ -251,12 +251,12 @@ get_header();
 			<div class="section-content-text align-left">
 				<div class="section-text">
 					<h2>Section Pro</h2>
-					<p>Destinée aux jeunes danseurs passionnés souhaitant se préparer au métier de danseur
-						professionnel, cette section propose un enseignement complet et exigeant, alliant qualité,
-						rigueur et transmission artistique. Dirigée par Roman Mikhalev et Corinne Lanssens,
-						tous deux issus de grandes écoles et riches de longues carrières sur les scènes
-						nationales et internationales, elle offre une formation solide, en adéquation avec
-						les attentes du milieu professionnel.</p>
+					<p>Destinée aux&nbsp;jeunes danseurs passionnés souhaitant se&nbsp;préparer au&nbsp;métier de&nbsp;danseur
+						professionnel, cette section propose un&nbsp;enseignement complet et&nbsp;exigeant, alliant qualité,
+						rigueur et&nbsp;transmission artistique. Dirigée par&nbsp;Roman Mikhalev et&nbsp;Corinne Lanssens,
+						tous deux issus de&nbsp;grandes écoles et&nbsp;riches de&nbsp;longues carrières sur&nbsp;les&nbsp;scènes
+						nationales et&nbsp;internationales, elle offre une&nbsp;formation solide, en&nbsp;adéquation avec
+						les&nbsp;attentes du&nbsp;milieu professionnel.</p>
 				</div>
 				<a class="btn btn-secondary-color" href="<?php echo get_permalink(131); ?>">Découvrir Section Pro</a>
 			</div>
@@ -309,14 +309,14 @@ get_header();
 				<div class="section-content-text align-left">
 					<div class="section-text">
 						<h2>Stages</h2>
-						<p>Le stage de danse est l’occasion de rencontrer de nouveaux professeurs, de découvrir
-							leur univers artistique, leur méthode et leur exigence pédagogique. C’est un moment
-							riche d’échanges, où l’on côtoie d’autres élèves venus de différentes écoles de danse,
-							créant ainsi une émulation supplémentaire. Grâce à l’intensité des cours sur plusieurs
-							jours, l’élève donne un nouvel élan à son apprentissage.</p>
-						<p>Durant la période de Stages, la <strong>Mikhalev Lanssens Ballet Academy</strong>
-                            tient à ce que chaque élève se sente considéré et puisse s’épanouir au sein de ses locaux.</p>
-                        <p>Pour consulter la liste complète des stages et leurs modalités, rendez-vous sur <a href="<?php echo site_url('/category/stages/'); ?>" title="Stages">Stages.</a></p>
+						<p>Le stage de&nbsp;danse est&nbsp;l’occasion de&nbsp;rencontrer de&nbsp;nouveaux professeurs, de&nbsp;découvrir
+							leur&nbsp;univers artistique, leur&nbsp;méthode et&nbsp;leur&nbsp;exigence pédagogique. C’est&nbsp;un&nbsp;moment
+							riche d’échanges, où&nbsp;l’on&nbsp;côtoie d’autres élèves venus de&nbsp;différentes écoles de&nbsp;danse,
+							créant ainsi une&nbsp;émulation supplémentaire. Grâce à&nbsp;l’intensité des&nbsp;cours sur&nbsp;plusieurs
+							jours, l’élève donne un&nbsp;nouvel élan à&nbsp;son&nbsp;apprentissage.</p>
+						<p>Durant la&nbsp;période de&nbsp;Stages, la&nbsp;<strong>Mikhalev Lanssens Ballet Academy</strong>
+                            tient à&nbsp;ce&nbsp;que&nbsp;chaque élève se&nbsp;sente considéré et&nbsp;puisse s’épanouir au&nbsp;sein de&nbsp;ses&nbsp;locaux.</p>
+                        <p>Pour consulter la&nbsp;liste complète des&nbsp;stages et&nbsp;leurs&nbsp;modalités, rendez-vous sur&nbsp;<a href="<?php echo site_url('/category/stages/'); ?>" title="Stages">Stages.</a></p>
 					</div>
 					<div class="stages-social">
 						<h4>Surveillez nos pages pour plus d'info</h4>
@@ -393,7 +393,7 @@ get_header();
 				<div class="section-content-text">
 					<div class="section-text">
 						<h2>Besoin d'aide pour choisir&nbsp;?</h2>
-						<p>Venez nous rencontrer&nbsp;: nous serons ravis de vous accueillir et de vous aider à choisir votre parcours.</p>
+						<p>Venez nous rencontrer&nbsp;: nous serons ravis de&nbsp;vous&nbsp;accueillir et&nbsp;de&nbsp;vous aider à&nbsp;choisir votre&nbsp;parcours.</p>
 						<div class="main-contacts-wrap">
 							<div class="three-items-wrap">
 								<div class="three-items-row">
